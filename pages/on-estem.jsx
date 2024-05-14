590ae2b291daffb1422b00674b0df9aac8b0d513
Pol Garcia <pol.garcia199710@gmail.com>
2024-05-14 18:48:12 +0200

    test01

--- pages/on-estem.jsx
+++ pages/on-estem.jsx
@@ -15,7 +15,7 @@ const Quisom = () => {
             <div className='bg-maize-100 flex flex-col  justify-center items-center gap-14' >
                 <div className='flex flex-row mt-16 mb-10 gap-10  shadow-lg p-10 w-[90vh]'>
                     <div className='flex flex-col  w-[60vh] gap-4'>
-                        <h1 className='text-[25px]  font-semibold '>ON ESTEM?</h1>
+                        <h1 className='text-[25px]  font-semibold '>ON ESEM?</h1>
                         <h1 className='text-[15px]  font-medium '> Ens podeu trobar a Carrer de Ramon Albó, 59, Nou Barris. 08027 Barcelona</h1>
                         <h1 className='text-[15px]  font-medium '> Per cualsevol dubte us podeu posar en contacte a traves del nostre mail jajajajdjq@gmail.com o del nostre telèfon +34 613 006 060</h1>
                     </div>
